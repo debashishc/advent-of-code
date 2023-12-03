bbb340a8d77310ecc9c76dfbd26d4c3be60d3469
debashishc <u4610248@anu.edu.au>
2023-12-03 16:12:36 +1100

    completed aoc day02 part 1: read the question dude, took longer than it should have

--- 2023/02/aoc02.py
+++ 2023/02/aoc02.py
@@ -4,105 +4,104 @@ def read_puzzle_input():
         return file.readlines()
 
 
-# Dictionary of letters and their number it represents between 0 and 9
-# e.g. "eight" => 8
-spelled_out_digits = {
-    "zero": "0",
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9",
-}
+# Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+# Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+# Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+# Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+# Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+# Sum of Correct Game Ids: 1 + 2 + 5 = 8
 
 
-def process_line(line):
-    # Find the indices of the first letter of each spelled out number
-    indices = find_spelled_out_numbers(line)
-
-    # Find the indices of all the digits
-    digits = find_digits(line)
-
-    # Concatenate the indices of the spelled out numbers and digits
-    indices += digits
-
-    # Sort the indices by their index
-    indices.sort(key=lambda x: x[1])
-
-    # Create a new line with digits and spelled out numbers according to their indices
-    new_line = ""
-    for index in indices:
-        # If the index is a spelled out number
-        if index[0] in spelled_out_digits.keys():
-            # Replace the spelled out number with its digit
-            new_line += spelled_out_digits[index[0]]
-        # If the index is a digit
-        else:
-            # Add the digit to the new line
-            new_line += index[0]
-    return new_line
-
-
-def sum_calibration_values(lines):
-    new_lines = [process_line(line) for line in lines]
-    print([extract_value(line) for line in new_lines])
-
-    # Summing the calibration values of each line
-    return sum(extract_value(line) for line in new_lines)
-
-
-def extract_value(line):
-    first_digit = next(char for char in line if char.isdigit())
-    last_digit = next(char for char in reversed(line) if char.isdigit())
-    return int(first_digit + last_digit)
-
-
-# Write a function that finds the spelled out numbers in a line and returns the word and indices of the first letter
-def find_spelled_out_numbers(line):
-    # Find the indices of the first letter of each spelled out number
-    word_indices = []
-    for word in spelled_out_digits:
-        index = line.find(word)
-        if index != -1:
-            # find all the indices of the word substring in the line using regex
-            import re
-
-            indices = [m.start() for m in re.finditer(word, line)]
-            word_indices += [(word, index) for index in indices]
-    return word_indices
-
-
-# Write a funtion that finds indices of all the digits in a line
-# return a list of tuples of the digit and its index
-def find_digits(line):
-    indices = []
-    for index, char in enumerate(line):
-        if char.isdigit():
-            indices.append((char, index))
-    return indices
-
-
+# Write a test for the function given above comment
 def test_sum_calibration_values():
     example_lines = [
-        "two1nine",
-        "eightwothree",
-        "abcone2threexyz",
-        "xtwone3four",
-        "4nineeightseven2",
-        "zoneight23 4",
-        "7pqrstsixteen",
-        "45122",
-        "sevenonezknqnkfqbzffjvfivetwo94two",
-        "mp7one6eightvhfnmfive6",
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
     ]
-    print(find_spelled_out_numbers("mp7one6eightvhfnmfive6"))
-    print(find_digits("mp7one6eightvhfnmfive6"))
-    print([process_line(line) for line in example_lines])
-    assert sum_calibration_values(example_lines) == 471
+    print([parse_line(line) for line in example_lines])
+    assert sum_filter_possible_games([parse_line(line) for line in example_lines]) == 8
+
+
+# Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
+
+# Find the number of cubes of each color in the bag
+# "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+# "1" : {"blue": 9, "red": 5, "green": 4}
+
+
+def parse_line(line):
+    # Find the index of the first colon
+    colon_index = line.index(":")
+
+    # Get the game id
+    game_id = line[:colon_index]
+
+    # Get the cubes
+    cubes = line[colon_index + 2 :]
+
+    # Split the cubes into the three games
+    games = cubes.split("; ")
+
+    # Split each game into the colors
+    games = [game.split(", ") for game in games]
+
+    # Split each color into the number of cubes
+    games = [[color.split(" ") for color in game] for game in games]
+
+    # Convert the number of cubes to integers
+    games = [{color[1]: int(color[0]) for color in game} for game in games]
+
+    return game_id.split(" ")[1], games
+
+
+# def sum_per_cube(game):
+#     """
+#     >>> sum_per_cube(('Game 1', [{'blue': 3, 'red': 4}, {'red': 1, 'green': 2, 'blue': 6}, {'green': 2}]))
+#     "1" : {"blue": 9, "red": 5, "green": 4}
+#     """
+#     game_id, games = game
+
+#     # Sum the number of cubes of each color
+#     sum_per_color = {}
+#     for game in games:
+#         for color in game:
+#             if color not in sum_per_color:
+#                 sum_per_color[color] = game[color]
+#             else:
+#                 sum_per_color[color] += game[color]
+
+#     return game_id.split(" ")[1], sum_per_color
+
+
+MAX_CUBES = {"red": 12, "green": 13, "blue": 14}
+
+
+# Write a function that determines if a game is possible
+def is_possible(game):
+    """
+    Check if the number of cubes of each color is less than or equal to the maximum number of cubes
+    >>> is_possible(('Game 1', {'blue': 9, 'red': 5, 'green': 4}))
+    True
+    >>> is_possible(('Game 2', {'blue': 15, 'green': 7, 'red': 5}))
+    False
+    """
+    game_id, list_of_games = game
+    for game in list_of_games:
+        for color in game:
+            if game[color] > MAX_CUBES[color]:
+                return False
+    return True
+
+
+def sum_filter_possible_games(games):
+    # return index of games that are possible
+    print([int(game[0]) for game in games if is_possible(game)])
+    return sum(
+        int(game[0]) for game in games if is_possible(game)
+    )  # sum of game ids that are possible
 
 
 if __name__ == "__main__":
@@ -110,4 +109,6 @@ if __name__ == "__main__":
     lines = read_puzzle_input()
 
     # Calculate the sum of calibration values
-    print(sum_calibration_values(lines))
+    # print([parse_line(line.strip()) for line in lines])
+
+    print(sum_filter_possible_games([parse_line(line.strip()) for line in lines]))
