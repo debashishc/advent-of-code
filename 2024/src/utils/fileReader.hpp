4205c5a5f50c736defe1c02e497b0302a37b4fa7
debashishc <u4610248@anu.edu.au>
2024-12-02 00:07:16 +1100

    aoc24 - day01 added TwoColumnData to read two column data

--- 2024/src/utils/fileReader.hpp
+++ 2024/src/utils/fileReader.hpp
@@ -3,10 +3,16 @@
 #include <vector>
 
 namespace utils {
+
+    struct TwoColumnData{
+        std::vector<int> left_column;
+        std::vector<int> right_column;
+    };
     class FileReader {
     public:
         static std::string readFile(const std::string& filename);
         static std::vector<std::string> readLines(const std::string& filename);
         static std::vector<int> readLinesAsInts(const std::string& filename);
+        static TwoColumnData readTwoColumnInts(const std::string& filename);
     };
 }
