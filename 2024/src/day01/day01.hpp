ebb844df3cd0a0c1f2f2dd92de7a5811aee60897
debashishc <u4610248@anu.edu.au>
2024-12-02 00:35:57 +1100

    aoc24 - edited data type for day01 static functions

--- 2024/src/day01/day01.hpp
+++ 2024/src/day01/day01.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "fileReader.hpp"  // Include this to get TwoColumnData definition
 
 class Day01 {
 public:
-    static int64_t solvePart1(const std::vector<std::string>& input);
-    static int64_t solvePart2(const std::vector<std::string>& input);
+    static int64_t solvePart1(const utils::TwoColumnData& input);
+    static int64_t solvePart2(const utils::TwoColumnData& input);
 };
--- 2024/src/utils/fileReader.hpp
+++ 2024/src/utils/fileReader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+// #include <utility>
 
 namespace utils {
 
