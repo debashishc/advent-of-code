ca4acbb96302c7ff18672688b5db3a6f936da0f4
debashishc <u4610248@anu.edu.au>
2024-12-09 02:17:12 +1100

    feat: day-2 part01 adding functionality to check if reports are safe checking

--- 2024/src/day02/day02.cpp
+++ 2024/src/day02/day02.cpp
@@ -1,23 +1,54 @@
 #include "day02.hpp"
-#include "fileReader.hpp"
+// #include "fileReader.hpp"
 #include <iostream>
+#include <string>
+#include <sstream>
 
-int64_t Day02::solvePart1(const std::vector<std::string>& input) {
+int64_t Day02::solvePart1(const utils::VariableColumnData& input) {
     // TODO: Implement solution for part 1
-    return 0;
+    auto rows = input.columns;
+    int safeReports = 0;
+
+    for (const auto& line : rows) {
+        bool isSafe = true;
+        bool isIncreasing = false;
+
+        if (line.size() >= 2) {
+            isIncreasing = line[1] > line[0];
+        }
+
+        for (size_t i = 1; i < line.size(); i++) {
+            int diff = line[i] - line[i-1];
+            bool validDiff = abs(diff) >= 1 && abs(diff) <= 3;
+
+            bool correctDirection = (isIncreasing && diff > 0) || (!isIncreasing && diff < 0);
+            
+            if (!validDiff || !correctDirection) {
+                isSafe = false;
+                break;
+            }
+        }
+
+        if (isSafe) {
+            safeReports++;
+        }
+
+    }
+
+    return safeReports;
 }
 
-int64_t Day02::solvePart2(const std::vector<std::string>& input) {
+int64_t Day02::solvePart2(const utils::VariableColumnData& input) {
     // TODO: Implement solution for part 2
     return 0;
 }
 
 int main() {
     try {
-        auto input = utils::FileReader::readLines("input.txt");
+        auto input = utils::FileReader::readVariableColumnInts("input.txt");
         
         std::cout << "Part 1: " << Day02::solvePart1(input) << std::endl;
-        std::cout << "Part 2: " << Day02::solvePart2(input) << std::endl;
+        // std::cout << "Part 2: " << Day02::solvePart2(input) << std::endl;
         
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
--- 2024/src/day02/day02.hpp
+++ 2024/src/day02/day02.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "fileReader.hpp"
 
 class Day02 {
 public:
-    static int64_t solvePart1(const std::vector<std::string>& input);
-    static int64_t solvePart2(const std::vector<std::string>& input);
+    static int64_t solvePart1(const utils::VariableColumnData& input);
+    static int64_t solvePart2(const utils::VariableColumnData& input);
 };
