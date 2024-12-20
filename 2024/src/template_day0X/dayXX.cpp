878022f484f447caefcb82c634e72dbe93e856e2
debashishc <u4610248@anu.edu.au>
2024-12-20 15:36:18 +1100

    dayXX template, made template files more general

--- 2024/src/template_day0X/dayXX.cpp
+++ 2024/src/template_day0X/dayXX.cpp
@@ -1,13 +1,13 @@
-#include "day01.hpp"
+#include "dayXX.hpp"
 #include "fileReader.hpp"
 #include <iostream>
 
-int64_t Day01::solvePart1(const std::vector<std::string>& input) {
+int64_t DayXX::solvePart1(const std::vector<std::string>& input) {
     // TODO: Implement solution for part 1
     return 0;
 }
 
-int64_t Day01::solvePart2(const std::vector<std::string>& input) {
+int64_t DayXX::solvePart2(const std::vector<std::string>& input) {
     // TODO: Implement solution for part 2
     return 0;
 }
@@ -16,8 +16,8 @@ int main() {
     try {
         auto input = utils::FileReader::readLines("input.txt");
         
-        std::cout << "Part 1: " << Day01::solvePart1(input) << std::endl;
-        std::cout << "Part 2: " << Day01::solvePart2(input) << std::endl;
+        std::cout << "Part 1: " << DayXX::solvePart1(input) << std::endl;
+        std::cout << "Part 2: " << DayXX::solvePart2(input) << std::endl;
         
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
