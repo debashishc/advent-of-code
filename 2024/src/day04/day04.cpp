f9ed77af2af9e102dc64b3f95591cd521b592eab
debashishc <u4610248@anu.edu.au>
2024-12-22 08:06:19 +1100

    day04: added variables for day04 attempt

--- 2024/src/day04/day04.cpp
+++ 2024/src/day04/day04.cpp
@@ -4,8 +4,22 @@
 
 int64_t Day04::solvePart1(const std::vector<std::string>& input) {
     // TODO: Implement solution for part 1
+    const std::string word = "XMAS";
+    int64_t count = 0;
     
-    return 0;
+    const int wordLength = word.length();
+    const int rows = input.size();
+    const int cols = input[0].size();
+    
+    // possible directions to search for the word
+    const std::vector<std::pair<int, int>> directions = {
+        {0, 1}, {0, -1}, {1, 0}, {-1,0},
+        {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+    };
+
+    
+    
+    return count;
 }
 
 int64_t Day04::solvePart2(const std::vector<std::string>& input) {
