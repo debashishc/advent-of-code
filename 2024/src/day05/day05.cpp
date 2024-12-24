d7cf1618395ed28153f91d674d1362f084b7cc8d
debashishc <u4610248@anu.edu.au>
2024-12-25 09:40:21 +1100

    day05: added template for day05

---
+++ 2024/src/day05/day05.cpp
@@ -0,0 +1,28 @@
+#include "day05.hpp"
+#include "fileReader.hpp"
+#include <iostream>
+
+int64_t Day05::solvePart1(const std::vector<std::string>& input) {
+    // TODO: Implement solution for part 1
+    return 0;
+}
+
+int64_t Day05::solvePart2(const std::vector<std::string>& input) {
+    // TODO: Implement solution for part 2
+    return 0;
+}
+
+int main() {
+    try {
+        auto input = utils::FileReader::readLines("input.txt");
+        
+        std::cout << "Part 1: " << Day05::solvePart1(input) << std::endl;
+        std::cout << "Part 2: " << Day05::solvePart2(input) << std::endl;
+        
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    
+    return 0;
+}
---
+++ 2024/src/day05/day05.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class Day05 {
+public:
+    static int64_t solvePart1(const std::vector<std::string>& input);
+    static int64_t solvePart2(const std::vector<std::string>& input);
+};
