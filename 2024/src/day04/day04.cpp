a7a0af1018dec5b64b10299c79c46a4e58fdf696
debashishc <u4610248@anu.edu.au>
2024-12-25 09:40:41 +1100

    fixed doc and naming for day04

--- 2024/src/day04/day04.cpp
+++ 2024/src/day04/day04.cpp
@@ -48,7 +48,7 @@ int64_t Day04::solvePart2(const std::vector<std::string>& input) {
     int rows = input.size();
     int cols = input[0].size();
 
-    auto isMas = [](char a, char b, char c) -> bool {
+    auto isValidMas = [](char a, char b, char c) -> bool {
         return (a == 'M' && b == 'A' && c == 'S') ||
         (a == 'S' && b == 'A' && c == 'M');
     };
@@ -56,8 +56,8 @@ int64_t Day04::solvePart2(const std::vector<std::string>& input) {
     for (int r = 1; r < rows - 1; ++r) {
         for (int c = 1; c < cols - 1; ++c) {
             if (input[r][c] == 'A') {
-                if (isMas(input[r - 1][c - 1], input[r][c], input[r + 1][c + 1]) &&
-                isMas(input[r - 1][c + 1], input[r][c], input[r + 1][c - 1])) {
+                if (isValidMas(input[r - 1][c - 1], input[r][c], input[r + 1][c + 1]) &&
+                isValidMas(input[r - 1][c + 1], input[r][c], input[r + 1][c - 1])) {
                     count++;
                 }
             }
