c47f29b876a26f04d318c72ec3d937ee2d295151
debashishc <u4610248@anu.edu.au>
2024-12-26 17:35:50 +1100

    corrected hpp class name

--- 2024/src/template_day0X/dayXX.hpp
+++ 2024/src/template_day0X/dayXX.hpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-class Day04 {
+class DayXX {
 public:
     static int64_t solvePart1(const std::vector<std::string>& input);
     static int64_t solvePart2(const std::vector<std::string>& input);
