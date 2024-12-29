3639c915e68b4d3073ec1aac3cb717c94cc0ec7e
debashishc <u4610248@anu.edu.au>
2024-12-30 07:41:17 +1100

    day05 part2 topological sort working

--- 2024/src/day05/day05.cpp
+++ 2024/src/day05/day05.cpp
@@ -82,30 +82,71 @@ int64_t Day05::solvePart1(const std::vector<std::string>& input) {
     return sumMiddlePages;
 }
 
+std::vector<int> _topologicalSort(const std::vector<int>& update,
+                                        const std::unordered_map<int, std::unordered_set<int>>& rules) {
+    std::vector<int> sortedPages;
+    std::unordered_map<int, int> inDegree;
+    std::unordered_map<int, std::vector<int>> graph;
+    for (int page : update) {
+        inDegree[page] = 0;
+    }
+    for (const auto& [before, afterSet] : rules) {
+        if (inDegree.count(before)) {
+            for (int after : afterSet) {
+                if (inDegree.count(after)) {
+                    graph[before].push_back(after);
+                    inDegree[after]++;
+                }
+            }
+        }
+    }
+
+    std::queue<int> q;
+    for (const auto& [page, degree] : inDegree) {
+        if (degree == 0) {
+            q.push(page);
+        }
+    }
+
+    while (!q.empty()) {
+        int current = q.front();
+        sortedPages.push_back(current);
+        q.pop();
+    
+        for (int neighbour : graph[current]) {
+            if (--inDegree[neighbour] == 0) {
+                q.push(neighbour);
+            }
+        }
+
+    }
+
+    return sortedPages;
+}
+
 int64_t Day05::solvePart2(const std::vector<std::string>& input) {
     // TODO: Implement solution for part 2
     int64_t sumMiddlePages = 0;
     std::unordered_map<int, std::unordered_set<int>> rules;
     std::vector<std::vector<int>> updates;
 
-    std::vector<std::vector<int>> invalidUpdates;
+    // std::vector<std::vector<int>> reorderedUpdates;
 
     parseInput(input, rules, updates);
 
     for (const auto& update : updates) {
-        // add invalid updates
+        // add reordered updates
         if (!isValidOrder(update, rules)) {
-            invalidUpdates.push_back(update);
+            auto reorderedUpdate = _topologicalSort(update, rules);
+            sumMiddlePages += reorderedUpdate[reorderedUpdate.size() / 2];
         }
     }
 
-    for (const auto& invalidUpdate : invalidUpdates) {
-        std::sort(invalidUpdate.begin(), invalidUpdate.end());
-        sumMiddlePages += invalidUpdate[invalidUpdate.size() / 2];
-    }
-
     return sumMiddlePages;
+
 }
+
+
 
 int main() {
     try {
